21e54e6ae5d056ce9e8841c37dbb33e76ddec8a1
t1dmlgus <dmlgusgngl@gmail.com>
2022-08-21 01:58:10 +0900

    Initial order-mvp2

---
+++ src/test/java/dev/t1dmlgus/ordermvp2/OrderMvp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.t1dmlgus.ordermvp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMvp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
