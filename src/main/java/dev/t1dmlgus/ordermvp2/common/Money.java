c811264c8a2f0b161be4e84576c86d8a1424186b
t1dmlgus <dmlgusgngl@gmail.com>
2022-08-25 00:45:06 +0900

    feat: Money 클래스(공통)

--- src/main/java/dev/t1dmlgus/ordermvp2/common/Money.java
+++ src/main/java/dev/t1dmlgus/ordermvp2/common/Money.java
@@ -1,9 +1,9 @@
 package dev.t1dmlgus.ordermvp2.common;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
-@EqualsAndHashCode
+import java.util.Objects;
+
 @Getter
 public class Money {
 
@@ -22,5 +22,16 @@ public class Money {
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return value == money.value;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
